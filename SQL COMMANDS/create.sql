c072fb2be944adfba189de7bf3863f725cdbefd9
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-10-29 16:45:25 +0530

    Update create.sql

--- SQL COMMANDS/create.sql
+++ SQL COMMANDS/create.sql
@@ -1,109 +1,113 @@
-create database library1;
-use library1;
-show tables;
-create table Department
+create database LIBRARY_DATABASE;
+use LIBRARY_DATABASE;
+
+create table Login(
+   sl_no int primary key,
+   name varchar(20),
+   t_date date,
+   entry_time time,
+   exit_time time,
+   Department_name varchar(30) 	
+);
+
+create table Department(
+    subject_id int primary key,
+    Sname varchar(20),
+    Department_id int,
+    Dname varchar(30),
+    total_books int ,
+    shelf_no int 
+);
+
+create table Vendors(
+    vendor_id  char(5),
+    vender_name varchar(20),
+    email varchar(20),
+    phone_no int(10),
+    no_of_books int, 
+    address text,
+    type enum('books','journals'),
+    amount float
+);
+
+create table category(
+    category_id int primary key,
+    category_name varchar(20),
+    course varchar(20)
+    branch varchar(30),
+);
+
+create table Books(
+    Book_id int primary key,
+    Book_title varchar(30),
+    Book_author varchar(20),
+    Book_edition int,
+    subject_id int ,
+    foreign key(subject_id) references Department(subject_id) 
+);
+    
+create table Members(
+    Member_id varchar(10) primary key,
+    Member_name varchar(20),
+    category_id int,
+    email varchar(20),
+    phone_no int(10),
+    address text,
+    expiry_data date
+    foreign key(category_id) references category(category_id)
+);
+
+create table Issue(
+        issue_id int(5) primary key,
+	Member_id varchar(10),
+        Book_id int,
+	foreign key(Member_id) references Members(Member_id),
+	foreign key(Book_id) references Books(Book_id),
+        issue_date date,
+	due_date date,
+        return_date date
+);
+    
+create table Journals(
+    journal_id int primary key,
+    journal_title varchar(20),
+    Language_name varchar(10),
+    frequency int,
+    vendor_id char(5),
+    foreign key(vendor_id) references Vendors(vendor_id)
+    );
+    
+create table EBooks(
+    ebook_id int primary key,
+    eBook_title varchar(20),
+    author varchar(20),
+    subject_id int,
+    url varchar(20),
+    ratings_value int,
+    format text,
+    foreign key(subject_id) references Department(subject_id)
+);
+    
+create table staff
 (
-  subject_id VARCHAR(5) PRIMARY KEY,
-  Sname text,
-  Department_id INT,
-  Dname VARCHAR(30),
-  total_books INT,
-  shelf_no INT
-);
-CREATE TABLE Books (
-    Book_id INT PRIMARY KEY,
-    Book_title text,
-    Book_author VARCHAR(20),
-    Book_edition INT,
-    categorised VARCHAR(5),
-    FOREIGN KEY (categorised) REFERENCES Department(subject_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-alter  table ebooks modify eBook_title text;
-
-
-CREATE TABLE EBooks (
-    ebook_id INT PRIMARY KEY,
-    eBook_title VARCHAR(20),
-    author VARCHAR(20),
-    category VARCHAR(5),
-    url text,
-    ratings_value INT,
-    format TEXT,
-    FOREIGN KEY (category) REFERENCES Department(subject_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE TABLE Staff (
-    staff_id VARCHAR(10) PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    gender ENUM('Male', 'Female', 'Others'),
-    designation VARCHAR(20),
-    phone_no BIGINT,
-    address TEXT,
-    joining_year INT
-);
-create TABLE mail_ids
-(
-  email_id varchar(30),
-  staff_id VARCHAR(10),
-  primary key(email_id,staff_id)
-);
-CREATE TABLE Login (
-   login_id INT PRIMARY KEY,
-   first_name VARCHAR(20),
-   last_name VARCHAR(20),
-   t_date DATE,
-   visit ENUM('reading', 'borrow', 'return'),
-   entry_time TIME,
-   exit_time TIME,
-   Department_name VARCHAR(30)
-);
-CREATE TABLE Issue (
-    issue_id INT PRIMARY KEY,
-    issue_date DATE,
-    due_date DATE,
-    return_date DATE
+    staff_id varchar(10) primary key,
+    staff_name varchar(20),
+    gender enum('Male','Female','others'),
+    designation varchar(20),
+    email varchar(20),
+    phone_no int(10),
+    address text,
+    joining_year int
 );
 
-CREATE TABLE Members (
-	 Member_id VARCHAR(10) PRIMARY KEY,
-     first_name VARCHAR(20),
-     last_name VARCHAR(20),
-     email VARCHAR(20),
-     phone_no BIGINT,
-     address TEXT,
-     next_renewal DATE,
-	 login_id INT ,
-     registeredby  VARCHAR(10),
-     FOREIGN KEY ( registeredby ) REFERENCES Staff(staff_id) ,
-     FOREIGN KEY (login_id) REFERENCES Login(login_id)
-);
-
-CREATE TABLE Fine (
-    Fine_id INT PRIMARY KEY,
-    Member_id VARCHAR(10),
-    issue_id INT,
-    fine_date DATE,
-    fine_amount INT,
-    fine_description TEXT,
-    fine_paid BOOLEAN,
-    FOREIGN KEY (issue_id) REFERENCES Issue(issue_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (Member_id) REFERENCES Members(Member_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-create table Access
-(
-    ebook_id INT ,
-    Member_id VARCHAR(10),
-    FOREIGN KEY (ebook_id) REFERENCES ebooks(ebook_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (Member_id) REFERENCES Members(Member_id) ON DELETE CASCADE ON UPDATE CASCADE
-); 
-
-create table borrows
-(
-	issue_id INT ,
-    Member_id VARCHAR(10),
-	Book_id INT,
-	FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (issue_id) REFERENCES issue(issue_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (Member_id) REFERENCES Members(Member_id) ON DELETE CASCADE ON UPDATE CASCADE
+create table Fine(
+    Fine_id int(5) primary key,
+    Member_id varchar(10),
+    issue_id int(5),
+    fine_date date,
+    fine_amount int,
+    fine_description text,
+    fine_paid boolean,
+    foreign key(issue_id) references Issue(issue_id),
+    foreign key(Member_id) references Members(Member_id)
 );
