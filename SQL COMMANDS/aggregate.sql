6b675f79553b946a314f35b9930dc3a9b43a87ed
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-22 23:23:14 +0530

    Create aggregate.sql

---
+++ SQL COMMANDS/aggregate.sql
@@ -0,0 +1,17 @@
+SELECT fine_description,SUM(fine_amount) AS total_amount
+FROM fine
+WHERE fine_date!='2023-10-17'
+GROUP BY fine_description
+HAVING SUM(fine_amount)>50
+ORDER BY total_amount DESC;
+
+
+SELECT eBook_title AS MAX_rating
+FROM Ebooks
+WHERE ratings_value = (SELECT MAX(ratings_value) FROM Ebooks);
+
+
+
+SELECT Fine_id,issue_id,Member_id FROM fine 
+where fine_amount<(
+select avg(fine_amount) from fine);
