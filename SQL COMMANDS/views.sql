99245809aa9b1c7c0ea102700de019f02e8c8471
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-22 23:19:48 +0530

    Update views.sql

--- SQL COMMANDS/views.sql
+++ SQL COMMANDS/views.sql
@@ -5,3 +5,11 @@ INNER JOIN Members ON Fine.Member_id = Members.Member_id
 WHERE Fine.fine_paid = 0;
 
 select * from FINEPAIDMEMBERS;
+
+
+CREATE view LoginMembersOnAday AS
+SELECT COUNT(*) AS num_users
+FROM Login
+WHERE t_date = '2023-10-24';
+
+select * from LoginMembersOnAday;
