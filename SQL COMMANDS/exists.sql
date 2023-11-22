db306f020117c8c7636568a4f57b06c4477dc9a8
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-22 23:49:11 +0530

    Update exists.sql

--- SQL COMMANDS/exists.sql
+++ SQL COMMANDS/exists.sql
@@ -1,3 +1,8 @@
 select * from issue where exists (
 select 1 from Fine where issue.issue_id=fine.issue_id );
 
+SELECT  member_id,first_name as name
+FROM members
+WHERE EXISTS (SELECT * FROM borrows where borrows.member_id= members.member_id)
+AND 
+EXISTS (SELECT * FROM fine where Fine.member_id= members.member_id);
