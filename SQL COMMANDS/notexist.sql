1222dc4da3499d2ad5ecda807df12c7d86861eae
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-23 00:31:54 +0530

    Update notexist.sql

--- SQL COMMANDS/notexist.sql
+++ SQL COMMANDS/notexist.sql
@@ -1,8 +1,9 @@
+SELECT Member_id,first_name as name
+FROM Members 
+WHERE NOT EXISTS(SELECT *FROM Borrows WHERE Members.Member_id = Borrows.Member_id);
+
+
 SELECT Member_id,first_name,last_name
 FROM Members
 WHERE NOT EXISTS ((SELECT member_id FROM Access WHERE Members.member_id = Access.Member_id) 
 EXCEPT (SELECT Member_id FROM Members WHERE first_name = "john"));
-
-SELECT Member_id,first_name as name
-FROM Members 
-WHERE NOT EXISTS(SELECT *FROM Borrows WHERE Members.Member_id = Borrows.Member_id);
