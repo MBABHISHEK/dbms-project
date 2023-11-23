7fe57de041e7861c68739d0b4121e49d81c86356
kirtanakiran <kirtana.k202@gmail.com>
2023-11-23 18:03:25 +0530

    new frontend

---
+++ FORNT END/library-app/src/App.js
@@ -0,0 +1,12 @@
+import AddBookForm from './components/AddBookForm';
+
+function App() {
+  return (
+    <diV>
+      <h1>BOOKS </h1>
+      <AddBookForm/>
+    </diV>
+  );
+}
+
+export default App;
