fbfea437076bfa677d0c806dce76b36faf563a57
SINCHANA S <139264130+SinchanaSmysore@users.noreply.github.com>
2023-11-23 10:45:30 +0530

    Update create.sql

--- SQL COMMANDS/create.sql
+++ SQL COMMANDS/create.sql
@@ -1,30 +1,35 @@
-CREATE TABLE Department (
-    subject_id VARCHAR(255) PRIMARY KEY,
-    Sname TEXT,
-    Department_id INT,
-    Dname VARCHAR(30),
-    total_books INT,
-    shelf_no INT
+create table Department
+(
+  subject_id VARCHAR(5) PRIMARY KEY,
+  Sname text,
+  Department_id INT,
+  Dname VARCHAR(30),
+  total_books INT,
+  shelf_no INT
 );
 
 CREATE TABLE Books (
     Book_id INT PRIMARY KEY,
-    Book_title TEXT,
+    Book_title text,
     Book_author VARCHAR(20),
+    Book_edition INT,
     categorised VARCHAR(5),
-    FOREIGN KEY (categorised) REFERENCES Department (subject_id)
+    FOREIGN KEY (categorised) REFERENCES Department(subject_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 
 CREATE TABLE EBooks (
     ebook_id INT PRIMARY KEY,
     eBook_title VARCHAR(20),
     author VARCHAR(20),
     category VARCHAR(5),
-    url TEXT,
+    url text,
     ratings_value INT,
     format TEXT,
-    FOREIGN KEY (category) REFERENCES Department (subject_id)
+    FOREIGN KEY (category) REFERENCES Department(subject_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+alter  table ebooks modify eBook_title text;
 
 CREATE TABLE Staff (
     staff_id VARCHAR(10) PRIMARY KEY,
@@ -37,42 +42,43 @@ CREATE TABLE Staff (
     joining_year INT
 );
 
-CREATE TABLE mail_ids (
-    email_id VARCHAR(30) PRIMARY KEY,
-    staff_id VARCHAR(10) PRIMARY KEY,
-    FOREIGN KEY (staff_id) REFERENCES Staff (staff_id)
+create TABLE mail_ids
+(
+  email_id varchar(30),
+  staff_id VARCHAR(10),
+  primary key(email_id,staff_id)
 );
 
 CREATE TABLE Login (
-    login_id INT PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    t_date DATE,
-    visit ENUM('reading', 'borrow', 'return'),
-    entry_time TIME,
-    exit_time TIME,
-    Department_name VARCHAR(30)
+   login_id INT PRIMARY KEY,
+   first_name VARCHAR(20),
+   last_name VARCHAR(20),
+   t_date DATE,
+   visit ENUM('reading', 'borrow', 'return'),
+   entry_time TIME,
+   exit_time TIME,
+   Department_name VARCHAR(30)
 );
 
 CREATE TABLE Issue (
-    issue_id INT PRIMARY KEY,
+	issue_id INT PRIMARY KEY,
     issue_date DATE,
     due_date DATE,
     return_date DATE
 );
 
 CREATE TABLE Members (
-    Member_id VARCHAR(10) PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    email VARCHAR(20),
-    phone_no BIGINT,
-    address TEXT,
-    next_renewal DATE,
-    login_id INT,
-    registeredby VARCHAR(10),
-    FOREIGN KEY (login_id) REFERENCES Login (login_id),
-    FOREIGN KEY (registeredby) REFERENCES Staff (staff_id)
+	 Member_id VARCHAR(10) PRIMARY KEY,
+     first_name VARCHAR(20),
+     last_name VARCHAR(20),
+     email VARCHAR(20),
+     phone_no BIGINT,
+     address TEXT,
+     next_renewal DATE,
+	 login_id INT ,
+     registeredby  VARCHAR(10),
+     FOREIGN KEY ( registeredby ) REFERENCES Staff(staff_id) ,
+     FOREIGN KEY (login_id) REFERENCES Login(login_id)
 );
 
 CREATE TABLE Fine (
@@ -83,22 +89,25 @@ CREATE TABLE Fine (
     fine_amount INT,
     fine_description TEXT,
     fine_paid BOOLEAN,
-    FOREIGN KEY (issue_id) REFERENCES Issue (issue_id),
-    FOREIGN KEY (Member_id) REFERENCES Members (Member_id)
+    FOREIGN KEY (issue_id) REFERENCES Issue(issue_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Member_id) REFERENCES Members(Member_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Access (
-    ebook_id INT,
-    Member_id VARCHAR(10),
-    FOREIGN KEY (ebook_id) REFERENCES EBooks (ebook_id),
-    FOREIGN KEY (Member_id) REFERENCES Members (Member_id)
-);
 
-CREATE TABLE borrows (
-    issue_id INT,
+create table Access
+(
+    ebook_id INT ,
     Member_id VARCHAR(10),
-    Book_id INT,
-    FOREIGN KEY (Book_id) REFERENCES Books (Book_id),
-    FOREIGN KEY (issue_id) REFERENCES Issue (issue_id),
-    FOREIGN KEY (Member_id) REFERENCES Members (Member_id)
+    FOREIGN KEY (ebook_id) REFERENCES ebooks(ebook_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Member_id) REFERENCES Members(Member_id) ON DELETE CASCADE ON UPDATE CASCADE
+); 
+
+create table borrows
+(
+	issue_id INT ,
+    Member_id VARCHAR(10),
+	Book_id INT,
+	FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (issue_id) REFERENCES issue(issue_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Member_id) REFERENCES Members(Member_id) ON DELETE CASCADE
 );
