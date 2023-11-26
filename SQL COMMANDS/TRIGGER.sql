f786731f1adcb4cc375dc7e5a11b287a108333ea
SINCHANA S <139264130+SinchanaSmysore@users.noreply.github.com>
2023-11-26 16:23:15 +0530

    Update TRIGGER.sql

--- SQL COMMANDS/TRIGGER.sql
+++ SQL COMMANDS/TRIGGER.sql
@@ -5,3 +5,17 @@ CREATE TRIGGER update_book_count
  SET total_books=total_books-1
  where department.subject_id=(
  SELECT subject_id FROM books where book_id=NEW.book_id);
+
+DELIMITER //
+CREATE TRIGGER UpdateFineAmountOnLateReturn
+AFTER UPDATE ON Issue
+FOR EACH ROW
+BEGIN
+    DECLARE fine_amount INT;
+    SET fine_amount = DATEDIFF(NEW.return_date, NEW.due_date) * 10;
+	UPDATE Fine
+    SET fine_amount = fine_amount
+    WHERE issue_id = NEW.issue_id;
+END;
+//
+DELIMITER ;
